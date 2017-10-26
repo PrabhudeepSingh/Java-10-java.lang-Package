59b6dc0ad8e80be8077058eccf87cebac1248fbe
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-26 21:19:20 +0530

    To illustrate the concept of parse methods in java.

---
+++ Parse.java
@@ -0,0 +1,15 @@
+class Parse
+{
+	public static void main(String[] args)
+	{
+		int i = Integer.parseInt("10");
+		float f = Float.parseFloat("10.5");
+		Double d = Double.parseDouble("10.5");
+		Boolean bool = Boolean.parseBoolean("true");
+		
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(bool);
+	}
+}

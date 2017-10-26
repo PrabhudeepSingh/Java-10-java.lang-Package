a7c209a1c403f586710e090493f6f0319f764d01
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-26 21:18:39 +0530

    To illustrate the concept of different Value methods presnt in wrapper classes in java.

---
+++ Value.java
@@ -0,0 +1,15 @@
+class Value
+{
+	public static void main(String[] args)
+	{
+		Integer i = Integer.valueOf(130);
+		
+		System.out.println(i.byteValue());
+		System.out.println(i.shortValue());
+		System.out.println(i.intValue());
+		System.out.println(i.longValue());
+		System.out.println(i.floatValue());
+		System.out.println(i.doubleValue());
+	}
+	
+}
